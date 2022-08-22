d5907b9f42527000181a7f5db4fed8d0bd510156
Tommy McG <thomasjmcgovern98@gmail.com>
2022-08-22 19:22:02 -0400

    added more functionality -- minor bug in saving

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,74 @@
-// import { v4 as uuidV4 } from "uuid" 
-// console.log(uuidV4());
-
+import { v4 as uuidV4 } from "uuid" 
+// Create a type for typescript
+type Task = {
+    id: string,
+    title: string,
+    completed: boolean,
+    createdAt: Date
+ }
+// Constants to store DOM elements
 const list = document.querySelector<HTMLUListElement>("#list");
-const taskForm = document.querySelector("#new-task-form");
-const taskTitle = document.querySelector("#new-task-title");
+const taskForm = document.getElementById("new-task-form") as HTMLFormElement | null;
+const taskTitle = document.querySelector<HTMLInputElement>("#new-task-title");
+
+// Array of Tasks to store tasks taken from local storage
+const tasks: Task[] = loadTasks()
+tasks.forEach(addListItem)
+
+// Upon submitting the form
+taskForm?.addEventListener("submit", event => {
+    event.preventDefault();
+
+    // Ensure the title has a value
+    if(taskTitle?.value == "" || taskTitle?.value == null) return
+
+    // Set the task constant to contain the appropriate values
+    const task: Task = {
+        id: uuidV4(),
+        title: taskTitle.value,
+        completed: false,
+        createdAt: new Date()
+    }
+    // Push this task into the task array
+    tasks.push(task);
+
+    // Call add list item and reset value in the input to empty
+    addListItem(task)
+    taskTitle.value = "";
+})
+
+// Adds a list item based on the form input
+function addListItem(task: Task) {
+    // Create elements to store the data we are displaying
+    const item = document.createElement("li")
+    const label = document.createElement("label")
+    const checkbox = document.createElement("input")
+
+    // If the checkbox is changed, update the task completed value
+    checkbox.addEventListener("change", () => {
+        task.completed = checkbox.checked
+    })
+
+    // Set checkbox to be a checkbox that matches the status of its completed status
+    checkbox.type = "checkbox"
+    checkbox.checked = task.completed
+
+    // Append the list item to the list
+    label.append(checkbox, task.title)
+    item.append(label)
+    list?.append(item)
+    // Save the new information into local storage
+    saveTasks();
+}
+
+// Saves the current tasks array to local storage 
+function saveTasks() {
+    localStorage.setItem("Tasks", JSON.stringify(tasks))
+}
+
+// Loads the tasks array from local storage, if there are none, return an empty array
+function loadTasks(): Task[] {
+    const taskJSON = localStorage.getItem("Tasks")
+    if (taskJSON == null) return []
+    return JSON.parse(taskJSON)
+}
